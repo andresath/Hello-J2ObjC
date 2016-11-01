eb851e633a7ea66f8e720d0c186c8c4cd00a6ef6
Hinz, Kayla <Kayla.Hinz@disney.com>
2016-11-01 19:48:21 -0400

    Setup fresh Swift Master Detail project

---
+++ ios/Hello-J2ObjC-iOS/Hello-J2ObjC-iOS/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Hello-J2ObjC-iOS
+//
+//  Created by Hinz, Kayla on 11/1/16.
+//  Copyright © 2016 Hinz, Kayla. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
